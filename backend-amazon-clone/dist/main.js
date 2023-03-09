bdf850046c7849455258d1ea524aeab710eec8d7
FilatovAE <filatovaleksander.job@gmail.com>
2023-03-09 20:40:10 +0300

    set up prisma

--- backend-amazon-clone/dist/main.js
+++ backend-amazon-clone/dist/main.js
@@ -1,12 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@nestjs/core");
-const app_module_1 = require("./app.module");
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+const core_1 = require('@nestjs/core')
+const app_module_1 = require('./app.module')
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.setGlobalPrefix('api');
-    app.enableCors();
-    await app.listen(4200);
+  const app = await core_1.NestFactory.create(app_module_1.AppModule)
+  app.setGlobalPrefix('api')
+  app.enableCors()
+  await app.listen(4200)
 }
-bootstrap();
+bootstrap()
 //# sourceMappingURL=main.js.map
